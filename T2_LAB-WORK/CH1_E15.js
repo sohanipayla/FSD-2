8dd86cc89369382887fb817c83f7463d059e5e9f
sohanipayla <sohanipayla@gmail.com>
2026-04-15 10:19:00 +0530

    Update CH1_E15.js

--- T2_LAB-WORK/CH1_E15.js
+++ T2_LAB-WORK/CH1_E15.js
@@ -1,27 +1,31 @@
-//develop a web application that simulates a college classroom entry system using middleware when a student access the classrout
-// first point log that the student enter the student enter the campus 2) verify whether the student has cvalid id proof 3)
-// if valid allow entry and display a welcome msg if not valid deny access
+// develop a web app that simulates a college classroom entry system using middleware when a student access the class routes 
+// i) Log that the student entered the campus
+// ii) Verify wheather the student have a valid id proof 
+// iii) If valid, allow entry and display a welcome message. If not valid deny access
 
-var express = require("express");
-var app = express();
-const logCampusEntry = (req, res, next) => {
-    console.log("Step 1: Student entered the campus.");
-    next();
-};
-const verifyID = (req, res, next) => {
-    const hasID = req.query.id; 
-    if (hasID === "true") {
-        console.log("Step 2: Valid ID proof found.");
-        next();
-    } else {
-        console.log("Step 2: Access Denied. No ID proof.");
-        res.status(403).send("<h1>Access Denied!</h1><p>Please show a valid ID proof to enter the classroom.</p>");
+const express = require("express")
+const app = express()
+const entryLog = (req,res,next)=>{
+    console.log("Student Entered in Campus")
+    next()
+}
+hasid=true
+const checkId = (req,res,next)=>{
+    if (hasid == true){
+	req.student="Siya";
+        console.log("ID verified");
     }
-};
-app.use(logCampusEntry,verifyID);
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome to the Classroom!</h1><p>Entry Granted.</p>");
-});
-app.listen(5000, () => {
-    console.log("running");
-});
+    else{
+        console.log("Not a valid id");
+    }
+    next()
+}
+
+app.use("/user",entryLog,checkId)
+app.get("/user",(req,res)=>{
+    res.send(`Welcome ${req.student} to class`)
+})
+app.listen(2000,()=>{
+    console.log("Running");
+    
+})
