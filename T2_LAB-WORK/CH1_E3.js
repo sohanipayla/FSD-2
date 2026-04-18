4cd687a76546a67dd330cb30868537578a2653b0
sohanipayla <sohanipayla@gmail.com>
2026-04-18 11:37:02 +0530

    Delete T2_LAB-WORK directory

--- T2_LAB-WORK/CH1_E3.js
+++
@@ -1,8 +0,0 @@
-var express=require("express");
-var app=express()
-app.get("/calender/:day/event/:ename",(req,res)=>{
-    res.send(req.params);
-})
-app.listen(6001,()=>{
-    console.log("server is runnign")
-})
