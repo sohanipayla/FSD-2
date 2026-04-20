2beec8a04ceb7f3ea67563a824840ed199841843
sohanipayla <sohanipayla@gmail.com>
2026-04-20 12:08:44 +0530

    Delete T2_LAB-WORK/CH5 directory

--- T2_LAB-WORK/CH5/1.js
+++
@@ -1,4 +0,0 @@
-express=require("express")
-app=express()
-app.use(express.static(__dirname,{index:"1.html"}))
-app.listen(5623)
--- T2_LAB-WORK/CH5/2.js
+++
@@ -1,4 +0,0 @@
-express=require("express")
-app=express()
-app.use(express.static("Frontend",{index:"2.html"}))
-app.listen(5624)
