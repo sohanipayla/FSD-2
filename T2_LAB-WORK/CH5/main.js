6e794ff8d784d2242c357560b012edecfa152ee9
sohanipayla <sohanipayla@gmail.com>
2026-04-25 12:12:36 +0530

    Add files via upload

---
+++ T2_LAB-WORK/CH5/main.js
@@ -0,0 +1,10 @@
+ express = require('express');
+ app = express();
+ api = require('./API'); 
+ app.use("/api",api)
+ app.listen(7899)
+
+//  output check
+// 1)localhost:7899/api
+// 2)localhost:7899/api/101
+// 3)localhost:7899/api/branch/it
