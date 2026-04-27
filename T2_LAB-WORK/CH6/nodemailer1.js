8a9ec9108648f71d8a5c22edc53fc9128cdc6c29
sohanipayla <sohanipayla@gmail.com>
2026-04-27 13:10:09 +0530

    Add files via upload

---
+++ T2_LAB-WORK/CH6/nodemailer1.js
@@ -0,0 +1,29 @@
+nm=require("nodemailer")
+express=require("express");
+app=express();
+app.get("/",(req,res)=>{
+    
+    trans=nm.createTransport({
+    host:"smtp.gmail.com",
+    port:465,
+    auth:{
+        user:"graphinity0301@gmail.com",
+        pass:"nbfu apma kqgr mjmb"
+    }
+})
+var mailOptions={
+    from:"graphinity0301@gmail.co",
+    to:"mahekkoshti2@gmail.com",
+    subject:"hello",
+    text:"Test Mail",
+    html:"Testing Node mailer,<h1>Effect of </h1>"
+}
+trans.sendMail(mailOptions,(err,info)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log(info)
+    res.send("Thank You")
+})
+})
+app.listen(7557);
