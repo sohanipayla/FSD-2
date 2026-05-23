089ca2e40546d28be21a06ab87fb4a5acbd2b773
sohanipayla <sohanipayla@gmail.com>
2026-05-23 13:24:07 +0530

    Add files via upload

---
+++ T3_LABWORK_FSD/CH7/PB312.jsx
@@ -0,0 +1,20 @@
+
+function PB312(props) {
+  const textStyle = {
+    backgroundColor: "green",
+    fontSize: "50px",
+    fontStyle: "italic",
+    textTransform: "uppercase",
+    padding: '20px', 
+    textAlign: 'center',
+
+  };
+
+  return (
+    <div>
+      <h1 style={textStyle}>This is an React Application</h1>
+    </div>
+  );
+}
+
+export default PB312;
